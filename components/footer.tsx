3a585dfbae1e846fa827cea73be64e6b64f8722b
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-08-01 18:39:15 +0700

    feat: Added link to footer logos

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,11 +13,15 @@ export default function Footer() {
             <span className="text-lg font-semibold text-opacity-100">Rifatmon</span>
           </div>
           <div className="flex gap-6">
-            <Button variant="ghost" size="icon" className="text-opacity-80 hover:text-opacity-100">
-              <Github className="h-5 w-5" />
+            <Button variant="ghost" size="icon" className="text-opacity-80 hover:text-opacity-100" asChild>
+              <Link href="https://github.com/rifatfauzan" target="_blank" rel="noopener noreferrer">
+                <Github className="h-5 w-5" />
+              </Link>
             </Button>
-            <Button variant="ghost" size="icon" className="text-opacity-80 hover:text-opacity-100">
-              <Linkedin className="h-5 w-5" />
+            <Button variant="ghost" size="icon" className="text-opacity-80 hover:text-opacity-100" asChild>
+              <Link href="https://www.linkedin.com/in/rifat-fauzan-0b648b2b0/" target="_blank" rel="noopener noreferrer">
+                <Linkedin className="h-5 w-5" />
+              </Link>
             </Button>
             <Button variant="ghost" size="icon" className="text-opacity-80 hover:text-opacity-100">
               <Mail className="h-5 w-5" />
