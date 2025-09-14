512a1b3b1216ac594da66c7d97bc201996c6f1b6
Rifat Fauzan <rifat.fauzan8@gmail.com>
2025-09-14 14:58:59 +0700

    feat: Changed background

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,40 @@ import Contact from "@/components/contact"
 import Footer from "@/components/footer"
 import Experiences from "@/components/experiences"
 import ScrollReveal from "@/components/scroll-reveal"
+import Silk from "@/components/Silk"
 
 export default function Home() {
   return (
-    <div
-      className="min-h-screen"
-      style={{
-        background: "radial-gradient(circle, #2563eb 1%, #000 99%)"
-      }}
-    >
-      <Navbar />
-      <main>
-        <Hero />
-        <ScrollReveal direction="up" delay={0.2}>
-          <TechStack />
-        </ScrollReveal>
-        <ScrollReveal direction="up" delay={0.3}>
-          <Experiences />
-        </ScrollReveal>
-        <ScrollReveal direction="up" delay={0.4}>
-          <Projects />
-        </ScrollReveal>
-        <ScrollReveal direction="up" delay={0.5}>
-          <Contact />
-        </ScrollReveal>
-      </main>
-      <Footer />
+    <div className="min-h-screen relative">
+          <div className="fixed inset-0 z-0">
+            <Silk
+              speed={3}
+              scale={0.5}
+              color="#1A0C50"
+              noiseIntensity={0.2}
+              rotation={2}
+            />
+          </div>
+      
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <Hero />
+          <ScrollReveal direction="up" delay={0.1}>
+            <TechStack />
+          </ScrollReveal>
+          <ScrollReveal direction="up" delay={0.1}>
+            <Experiences />
+          </ScrollReveal>
+          <ScrollReveal direction="up" delay={0.1}>
+            <Projects />
+          </ScrollReveal>
+          <ScrollReveal direction="up" delay={0.1}>
+            <Contact />
+          </ScrollReveal>
+        </main>
+        <Footer />
+      </div>
     </div>
   )
 }
